8cfafdf1443a41df1125543f3230bd340f5b7ae7
德罗巴-王一舟 <wangyizhou@holomatic.com>
2024-02-16 22:16:13 -0800

    Add: feature matcher

--- sift.py
+++ sift.py
@@ -11,8 +11,16 @@ img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYP
 num_features, feature_dim = des.shape
 
 print(
-    f"Detected num of features = {num_features}, each feature vector has {feature_dim} dimensions"
+    f"Detected num of features = {num_features}, each feature vector has dimensions = {feature_dim}"
 )
+
+# Use BRIEF feature descriptor
+# Initiate BRIEF extractor
+brief = cv.xfeatures2d.BriefDescriptorExtractor_create()
+# compute the descriptors with BRIEF
+# which is binary string descriptors
+kp_brief, des_brief = brief.compute(img, kp)
+print(f"BRIEF has a reduced feature dimensions = {des_brief.shape[1]}")
 
 plt.imshow(img)
 plt.show()
