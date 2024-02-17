cdc2efd71a6fc4c3ad58a75cb28433540b90ee40
德罗巴-王一舟 <wangyizhou@holomatic.com>
2024-02-16 22:36:06 -0800

    Add: feature matcher then find homography

--- feature_matcher.py
+++ feature_matcher.py
@@ -18,9 +18,13 @@ matches = flann.knnMatch(des1, des2, k=2)
 # Need to draw only good matches, so create a mask
 matchesMask = [[0, 0] for i in range(len(matches))]
 # ratio test as per Lowe's paper
+good = []
+
 for i, (m, n) in enumerate(matches):
     if m.distance < 0.7 * n.distance:
         matchesMask[i] = [1, 0]
+        good.append(m)
+
 draw_params = dict(
     matchColor=(0, 255, 0),
     singlePointColor=(255, 0, 0),
@@ -31,3 +35,22 @@ img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
 plt.imshow(
     img3,
 ), plt.show()
+
+
+src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+matchesMask = mask.ravel().tolist()
+h, w = img1.shape
+pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+dst = cv.perspectiveTransform(pts, M)
+img2 = cv.polylines(img2, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+
+draw_params = dict(
+    matchColor=(0, 255, 0),  # draw matches in green color
+    singlePointColor=None,
+    matchesMask=matchesMask,  # draw only inliers
+    flags=2,
+)
+img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+plt.imshow(img3, "gray"), plt.show()
